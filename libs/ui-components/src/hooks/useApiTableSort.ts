19ee90c581fabeb2c25b4d6a9d80d766f204f38c
Celia Amador <camadorg@redhat.com>
2024-10-11 13:15:44 +0200

    EDM-593: Initial UI adaptations to new filtering and sorting API mechanisms

---
+++ libs/ui-components/src/hooks/useApiTableSort.ts
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { ThProps } from '@patternfly/react-table';
+
+import { ApiSortTableColumn } from '../components/Table/Table';
+
+const getDefaultSortField = (columns: ApiSortTableColumn[]) => {
+  let defaultSortCol = columns.find((c) => c.defaultSort);
+  if (!defaultSortCol) {
+    defaultSortCol = columns.find((c) => !!c.sortableField);
+  }
+  return defaultSortCol?.sortableField ||  '';
+};
+
+export const useApiTableSort = (columns: ApiSortTableColumn[]) => {
+  const [activeSortField, setActiveSortField] = React.useState<string>(getDefaultSortField(columns));
+  const [activeSortDirection, setActiveSortDirection] = React.useState<'Asc' | 'Desc'>('Asc');
+
+  const getSortParams = React.useCallback(
+    (columnIndex: number): ThProps['sort'] => {
+      const columnData = columns[columnIndex];
+      if (!columnData.sortableField) {
+        return undefined;
+      }
+
+      const activeColumnIndex = columns.findIndex((column) => column.sortableField === activeSortField);
+      return {
+        sortBy: {
+          index: activeColumnIndex === -1 ? 0 : activeColumnIndex,
+          direction: activeSortDirection === 'Asc' ? 'asc' :'desc',
+          defaultDirection: 'asc',
+        },
+        onSort: (_, index, direction) => {
+          const column = columns[index];
+          setActiveSortField(column.sortableField || '');
+          setActiveSortDirection(direction === 'asc' ? 'Asc' : 'Desc');
+        },
+        columnIndex,
+      };
+    },
+    [columns, activeSortField, activeSortDirection],
+  );
+
+  return { getSortParams, activeSortQuery: activeSortField ? `sortBy=${activeSortField}&sortOrder=${activeSortDirection}` : '' };
+};
